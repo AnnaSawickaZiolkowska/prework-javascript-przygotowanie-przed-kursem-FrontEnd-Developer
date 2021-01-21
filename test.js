b3240e9199aee95cd11fe4e599f36dcb7425b417
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-01-21 22:55:18 +0100

    Fuctions

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-var links = document.getElementsByTagName("a");
+/*
+function getAverage (a,b){
 
-for (i = 0; i < links.length; i++){
+    var average = (a + b) /2;
+    console.log(average);
 
-    links[1].className = "Link-" + i;
-    
 }
+
+getAverage(7,12);
+
+*/
+
+
+function getAverage (a,b, c, d, e, f){
+
+    var average = (a + b + c + d + e + f) /2;
+    console.log(average);
+    return average;
+}
+
+var myResult = getAverage(7,8,9,10,11,12);
+console.log("the averege is " + myResult);
