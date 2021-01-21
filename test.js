14d7c52db4964f5a4759ba7db8c07e4d6233ec93
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-01-21 21:48:24 +0100

    Else If Statement

--- test.js
+++ test.js
@@ -42,3 +42,23 @@ if (youLikeMeat){
     document.write("you hate meat")
 
 }
+
+
+var myAge = 29;
+
+if (myAge > 30){
+
+    document.write(You are over 30);
+
+} else if (myAge > 20) {
+
+    document.write("you are over 20");
+
+} else if (myAge > 10){
+
+    document.write(you are over 10);
+
+} else {
+
+    document.write("you are not over 10");
+}
