507bf4b92cbc1b454ff43ab0a83c6de176b28f31
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-01-21 22:29:47 +0100

    Break & continue

--- test.js
+++ test.js
@@ -25,6 +25,10 @@ console.log("Your age is less than 10");
 document.write("You are now over 10");
 */
 
+
+
+
+/*
 var links = document.getElementsByTagName("a");
 
 
@@ -34,3 +38,20 @@ for (i = 0; i <= links.length; i++){
     }
     
     document.write("All links now looped");
+    */
+
+    for (i = 0; i < 10; i++){
+        if (i === 5 || i ===3){
+
+            continue;
+        }
+
+        console.log(i);
+        
+        if (i === 7){
+
+            break;
+        }
+    }
+
+    console.log("I have broken out of the loop");
