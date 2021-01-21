f19c767c3fd5de131beb117501dff54ee0d4b464
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-01-21 21:40:53 +0100

    If Statements

--- test.js
+++ test.js
@@ -11,3 +11,34 @@ tekstu */
 // move the image from top to botteom
 //make the image invisible
 //make the image visible
+
+/* komendy
+myVar = 10
+myVar = myVar + 5   to samo co -> myVar += 5
+myVar++ -> dodaje 1
+
+*/
+
+var youLikeMeat = false;
+var myNum = 8;
+if (myNum == 10){
+
+    document.write("myNum is equal then 10")
+
+} else{
+
+    document.write("myNum is not equal to 10")
+
+}
+
+var youLikeMeat = true;
+var myNum = 8;
+if (youLikeMeat){
+
+    document.write("you like meat")
+
+} else{
+
+    document.write("you hate meat")
+
+}
