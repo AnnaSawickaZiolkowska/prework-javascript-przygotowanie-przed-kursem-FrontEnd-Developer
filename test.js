f062ff65520232ba3181627db55436de3d0b6bea
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-01-21 19:23:20 +0100

    komentarze

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
+//komentarze
+
+
+// the first alert
 alert("Hello, World");
+
+/* koemetarze na dłusze opisy 
+i pare linijek 
+tekstu */
+
+// move the image from top to botteom
+//make the image invisible
+//make the image visible
