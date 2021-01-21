8398efefaa6d242013c096adfe12f3151edcb360
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-01-21 22:03:27 +0100

    Logical Operators

--- test.js
+++ test.js
@@ -1,64 +1,24 @@
-//komentarze
+/* 
+var myAge = 25;
 
+if (myAge >= 18 && myAge <=30){     //&& and
 
-// the first alert
-alert("Hello, World");
-
-/* koemetarze na dłusze opisy 
-i pare linijek 
-tekstu */
-
-// move the image from top to botteom
-//make the image invisible
-//make the image visible
-
-/* komendy
-myVar = 10
-myVar = myVar + 5   to samo co -> myVar += 5
-myVar++ -> dodaje 1
-
-*/
-
-var youLikeMeat = false;
-var myNum = 8;
-if (myNum == 10){
-
-    document.write("myNum is equal then 10")
+    document.write("You can come, you cool dude");
 
 } else{
 
-    document.write("myNum is not equal to 10")
+    document.write("you aint coming");
+} */
 
-}
+var myAge = 35;
 
-var youLikeMeat = true;
-var myNum = 8;
-if (youLikeMeat){
+if (myAge < 18 || myAge > 30 || myAge === 25){     // || or tylko jedno stwierdzenie musi być prawdziwe
 
-    document.write("you like meat")
+    document.write("you aint coming");
+
 
 } else{
 
-    document.write("you hate meat")
+    document.write("You can come, you cool dude");
 
-}
-
-
-var myAge = 29;
-
-if (myAge > 30){
-
-    document.write(You are over 30);
-
-} else if (myAge > 20) {
-
-    document.write("you are over 20");
-
-} else if (myAge > 10){
-
-    document.write(you are over 10);
-
-} else {
-
-    document.write("you are not over 10");
 }
