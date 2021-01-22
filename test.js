e09afe2740d42d67e83fbde31fe0fd59e1abe331
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2021-01-22 14:29:40 +0100

    Variable scope

--- test.js
+++ test.js
@@ -1,16 +1,4 @@
 /*
-function getAverage (a,b){
-
-    var average = (a + b) /2;
-    console.log(average);
-
-}
-
-getAverage(7,12);
-
-*/
-
-
 function getAverage (a,b, c, d, e, f){
 
     var average = (a + b + c + d + e + f) /2;
@@ -20,3 +8,62 @@ function getAverage (a,b, c, d, e, f){
 
 var myResult = getAverage(7,8,9,10,11,12);
 console.log("the averege is " + myResult);
+
+*/
+
+
+/*
+function getAverage (a,b){
+
+    var average = (a + b) /2;
+    console.log(average);
+    return average;
+
+}
+
+var myResult = getAverage(7,11);  //global variable
+console.log("the averege is " + myResult);
+
+*/
+
+
+/* 
+// 1 - z average local
+
+function getAverage (a,b){
+
+    var average = (a + b) /2;
+    console.log(average);
+    return average;
+
+}
+
+var myResult = getAverage(7,11);  //global variable
+
+function logResult(){
+    console.log("the averege is " + myResult + " inside the function");
+
+}
+
+logResult();
+*/
+// z average global
+
+var average = 0
+
+function getAverage (a,b){
+
+    average = (a + b) /2;
+    console.log(average);
+    return average;
+
+}
+
+var myResult = getAverage(7,11);  //global variable
+
+function logResult(){
+    console.log("the averege is " + myResult + " inside the function");
+
+}
+
+logResult();
